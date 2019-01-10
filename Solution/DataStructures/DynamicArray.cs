330a980671e567070d06142cc8009d7300d5eff0
Phil Wright <phil.wright@evenglide.com.au>
2019-01-10 17:03:06 +1100

    DynamicArray
    DynamicArray

---
+++ Solution/DataStructures/DynamicArray.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace DataStructures
+{
+    // Pro:
+    //     Variable size
+    //     Fast lookup
+    //     Cache friendly, data is in a contiguous block
+    // Con:
+    //     Append has slow worst case
+    //     Insert/Delete are slow
+    //
+    // Notes:
+    //     Double in size when full 
+    //     Reduce in half when quarter full (not at half full, to avoid hysteresis)
+    //     No wasted space because of linked list type pointers, but has wasted space from empty entries
+    //
+    public class DynamicArray<T>
+    {
+        private T[] _storage;
+
+        public DynamicArray(int capacity = 1)
+        {
+            if (capacity < 1)
+                throw new ArgumentOutOfRangeException();
+
+            Capacity = capacity;
+            _storage = new T[Capacity];
+        }
+
+        public int Length { get; private set; }
+        public int Capacity { get; private set; }
+
+        // O(1)
+        public T this[int position]
+        {
+            get
+            {
+                if ((position < 0) || (position >= Length))
+                    throw new IndexOutOfRangeException();
+
+                return _storage[position];
+            }
+
+            set
+            {
+                if ((position < 0) || (position >= Length))
+                    throw new IndexOutOfRangeException();
+
+                _storage[position] = value;
+            }
+        }
+
+        // O(1) - average case
+        // O(n) - worst case
+        public void Append(T item)
+        {
+            CheckForSpaceToAddOne();
+            _storage[Length++] = item;
+        }
+
+        // O(n)
+        public void Insert(int position, T item)
+        {
+            if ((position < 0) || (position > Length))
+                throw new IndexOutOfRangeException();
+
+            CheckForSpaceToAddOne();
+
+            for (int i = (Length - 1); i >= position; i--)
+                _storage[i + 1] = _storage[i];
+
+            _storage[position] = item;
+            Length++;
+        }
+
+        // O(n)
+        public void Delete(int position)
+        {
+            if ((position < 0) || (position >= Length))
+                throw new IndexOutOfRangeException();
+
+            for (int i = position; i < (Length - 1); i++)
+                _storage[i] = _storage[i + 1];
+
+            Length--;
+            CheckForShrink();
+        }
+
+        private void CheckForSpaceToAddOne()
+        {
+            // Is there space to add another item?
+            if (Length == Capacity)
+            {
+                Capacity *= 2;
+
+                T[] doubled = new T[Capacity];
+                for (int i = 0; i < _storage.Length; i++)
+                    doubled[i] = _storage[i];
+
+                _storage = doubled;
+            }
+        }
+
+        private void CheckForShrink()
+        {
+            // If only a quarter full, then shrink down
+            if ((Capacity > 1) && (Length <= (Capacity / 4)))
+            {
+                Capacity /= 2;
+
+                T[] halfed = new T[Capacity];
+                for (int i = 0; i < Length; i++)
+                    halfed[i] = _storage[i];
+
+                _storage = halfed;
+            }
+        }
+    }
+}
---
+++ Solution/UnitTesting/UnitTestDynamicArray.cs
@@ -0,0 +1,237 @@
+using DataStructures;
+using System;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class UnitTestDynamicArray
+    {
+        [Fact]
+        public void Initial()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            Assert.Equal(0, da.Length);
+            Assert.Equal(1, da.Capacity);
+        }
+
+        [Fact]
+        public void ExpandingCapacity()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+            Assert.Equal(1, da.Capacity);
+
+            da.Append(1);
+            Assert.Equal(1, da.Length);
+            Assert.Equal(1, da.Capacity);
+
+            da.Append(2);
+            Assert.Equal(2, da.Length);
+            Assert.Equal(2, da.Capacity);
+
+            da.Append(3);
+            Assert.Equal(3, da.Length);
+            Assert.Equal(4, da.Capacity);
+
+            da.Append(4);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(4, da.Capacity);
+
+            da.Append(5);
+            Assert.Equal(5, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Append(6);
+            Assert.Equal(6, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Append(7);
+            Assert.Equal(7, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Append(8);
+            Assert.Equal(8, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Append(9);
+            Assert.Equal(16, da.Capacity);
+        }
+
+        [Fact]
+        public void ShrinkingCapacity()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            for (int i = 0; i < 8; i++)
+                da.Append(i);
+            Assert.Equal(8, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(7, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(6, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(5, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(3, da.Length);
+            Assert.Equal(8, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(2, da.Length);
+            Assert.Equal(4, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(1, da.Length);
+            Assert.Equal(2, da.Capacity);
+
+            da.Delete(0);
+            Assert.Equal(0, da.Length);
+            Assert.Equal(1, da.Capacity);
+        }
+
+        [Fact]
+        public void AppendLookup()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            da.Append(1);
+            Assert.Equal(1, da.Length);
+            Assert.Equal(1, da[0]);
+
+            da.Append(2);
+            Assert.Equal(2, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(2, da[1]);
+
+            da.Append(3);
+            Assert.Equal(3, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+
+            da.Append(4);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(4, da[3]);
+        }
+
+        [Fact]
+        public void InsertLookup()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            da.Insert(0, 10);
+            Assert.Equal(1, da.Length);
+            Assert.Equal(10, da[0]);
+
+            da.Insert(1, 11);
+            Assert.Equal(2, da.Length);
+            Assert.Equal(10, da[0]);
+            Assert.Equal(11, da[1]);
+
+            da.Insert(0, 12);
+            Assert.Equal(3, da.Length);
+            Assert.Equal(12, da[0]);
+            Assert.Equal(10, da[1]);
+            Assert.Equal(11, da[2]);
+
+            da.Insert(1, 13);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(12, da[0]);
+            Assert.Equal(13, da[1]);
+            Assert.Equal(10, da[2]);
+            Assert.Equal(11, da[3]);
+        }
+
+        [Fact]
+        public void DeleteLookup()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            da.Append(1);
+            da.Append(2);
+            da.Append(3);
+            da.Append(4);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(4, da[3]);
+
+            da.Delete(1);
+            Assert.Equal(3, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(3, da[1]);
+            Assert.Equal(4, da[2]);
+
+            da.Delete(2);
+            Assert.Equal(2, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(3, da[1]);
+
+            da.Delete(0);
+            Assert.Equal(1, da.Length);
+            Assert.Equal(3, da[0]);
+
+            da.Delete(0);
+            Assert.Equal(0, da.Length);
+        }
+
+        [Fact]
+        public void UpdateLookup()
+        {
+            DynamicArray<int> da = new DynamicArray<int>();
+
+            da.Append(1);
+            da.Append(2);
+            da.Append(3);
+            da.Append(4);
+            Assert.Equal(4, da.Length);
+            Assert.Equal(1, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(4, da[3]);
+
+            da[0] = 99;
+            Assert.Equal(4, da.Length);
+            Assert.Equal(99, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(4, da[3]);
+
+            da[3] = 98;
+            Assert.Equal(4, da.Length);
+            Assert.Equal(99, da[0]);
+            Assert.Equal(2, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(98, da[3]);
+
+            da[1] = 97;
+            Assert.Equal(4, da.Length);
+            Assert.Equal(99, da[0]);
+            Assert.Equal(97, da[1]);
+            Assert.Equal(3, da[2]);
+            Assert.Equal(98, da[3]);
+
+            da[2] = 96;
+            Assert.Equal(4, da.Length);
+            Assert.Equal(99, da[0]);
+            Assert.Equal(97, da[1]);
+            Assert.Equal(96, da[2]);
+            Assert.Equal(98, da[3]);
+        }
+    }
+}
